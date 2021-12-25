20c579e1d18d77bd72dcf53cf4a658a2875b67c1
Your Name <ubaydullayev@samtuit.uz>
2021-12-25 21:18:17 +0500

    1-misol5

--- get_length.py
+++ get_length.py
@@ -2,23 +2,23 @@
 def get_length(num):
     num1 = num % 10
     num //= 10 
-    num1 -= num1 - 1  
+    num1 -= abs(num1 - 1) - 1
    
     num2 = num % 10
     num //= 10 
-    num2 -= num2 - 1
+    num2 -= abs(num2 - 1) - 1
     
     num3 = num % 10
     num //= 10 
-    num3 -= num3 - 1
+    num3 -= abs(num3 - 1) - 1
     
     num4 = num % 10
     num //= 10 
-    num4 -= num4 - 1
+    num4 -= abs(num4 - 1) - 1
    
     num5 = num % 10
     num //= 10 
-    num5 -= num5 - 1       
+    num5 -= abs(num5 - 1 ) - 1      
     """
     Get length of integer
 
@@ -29,6 +29,6 @@ def get_length(num):
         int: length of integer
     """
     # return number of digits in integer
-    return num1 + num2 + num3 + num4 + num5
+    return int((num1 + num2 + num3 + num4 + num5)/2)
 
  
