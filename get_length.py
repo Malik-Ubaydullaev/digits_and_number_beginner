65ba38d796b9986e9cb5e26fb659e43efdafec72
Your Name <ubaydullayev@samtuit.uz>
2021-12-26 19:27:15 +0500

    1-misol7

--- get_length.py
+++ get_length.py
@@ -1,5 +1,7 @@
 #get number of digits in an int?
+from math import log10
 def get_length(num):
+    lengggg = log10(num) + 1
     num1 = num % 10
     num //= 10 
     num1 -= abs(num1 - 1) - 1
@@ -18,7 +20,9 @@ def get_length(num):
    
     num5 = num % 10
     num //= 10 
-    num5 -= abs(num5 - 1 ) - 1      
+    num5 -= abs(num5 - 1 ) - 1  
+    
+       
     """
     Get length of integer
 
@@ -29,6 +33,6 @@ def get_length(num):
         int: length of integer
     """
     # return number of digits in integer
-    return int((num1 + num2 + num3 + num4 + num5)/2)
+    return int(lengggg)
 
  
